2427058e2b0fc45ddcc3bb17ecc13f9d868b2730
Jessica Stehlin (JASTEH3941) <JASTEH3941@ung.edu>
2016-12-11 10:59:16 -0500

    Filter data and save as output file

--- sitepy.py
+++ sitepy.py
@@ -1,4 +1,4 @@
-# module run time: approx. 1 minute
+# module run time: less than 1 minute
 
 import csv
 import urllib
@@ -6,9 +6,10 @@ import urllib
 # open GASF file from url and save as csv
 urllib.urlretrieve('http://artiraq.org/static/opencontext/revised-tables/02f748469252f6d14250cb0c0b9d9f1e.csv' , "gasf_sitepy.csv")
 
-# filter only Hall County data and print results
-with open('gasf_sitepy.csv', 'rb') as f:
-    reader = csv.reader(f)
-    for row in reader:
+# filter only Hall County data and save to new output file
+with open('gasf_sitepy.csv','r') as i, open('gasf_hall.csv','w') as o:
+    r = csv.reader(i)
+    w = csv.writer(o)
+    for row in r:
         if row[15] == 'Hall':
-            print row
+            w.writerow(row)
