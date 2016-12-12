3cea038f583c610a047046bc5ca62079cdf41920
Jessica Stehlin (JASTEH3941) <JASTEH3941@ung.edu>
2016-12-12 10:07:16 -0500

    Count total number of Hall County sites

--- sitepy.py
+++ sitepy.py
@@ -4,9 +4,11 @@ import csv
 import urllib
 
 # open GASF file from url and save as csv
+
 urllib.urlretrieve('http://artiraq.org/static/opencontext/revised-tables/02f748469252f6d14250cb0c0b9d9f1e.csv' , "gasf_sitepy.csv")
 
 # filter only Hall County data and save to new output file
+
 with open('gasf_sitepy.csv','r') as i, open('gasf_hall.csv','w') as o:
     r = csv.reader(i)
     w = csv.writer(o)
@@ -15,9 +17,19 @@ with open('gasf_sitepy.csv','r') as i, open('gasf_hall.csv','w') as o:
             w.writerow(row)
 
 # filter only Endangered sites and save to new output file
+
 with open('gasf_sitepy.csv','r') as i, open('gasf_endangered.csv','w') as o:
     r = csv.reader(i)
     w = csv.writer(o)
     for row in r:
         if row[36] == 'Endangered' or row[36] == 'Preservation Prospects':
             w.writerow(row)
+
+# count total number of Hall County sites
+
+my_reader = csv.reader(open('gasf_sitepy.csv'))
+ctr = 0
+for record in my_reader:
+    if record[15] == 'Hall':
+        ctr += 1
+print(ctr)
